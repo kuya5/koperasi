7ad1a155267ddfad851c604feb14ccc16475a8ef
kuya5 <rizky.muchniardi@gmail.com>
2010-10-11 15:56:03 +0700

    Koperasi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koperasi::Application.config.secret_token = '7ab0346e89c76339ec654aa2e89cb135b4fe21ebb6c4de762e33ddbc7c88952b55844f153c01804b08498ebab238dd20dda37a81d57f239834ab495b6f49f66a'
